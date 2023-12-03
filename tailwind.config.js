6324410d9510ce709b5dd8726f8f17b5b49edd56
Kiran Ahmad <kiranahmad.freelancer@gmail.com>
2023-12-03 10:08:38 +0500

    TailwindCss task

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        fontFamily: {
+            'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+        },
+        color:{
+          blue:"#fff"
+        }
+    },
+},
+  plugins: [],
+}
+
